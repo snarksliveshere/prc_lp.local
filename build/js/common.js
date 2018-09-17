ced3c5ea6ca0347e5ed6353f41cacdcba9a31a64
snarksliveshere <snarksliveshere@gmail.com>
2018-09-17 17:32:13 +0300

    variant 2 markup

--- build/js/common.js
+++ build/js/common.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
 
     var body = $('body');
+
+    // sticky header
+    function fixedPanel() {
+        var doc = document.documentElement;
+        var scrollTop = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+        var offsetTop = $('.header__top').position().top + $('.header__top').height();
+        if (scrollTop > offsetTop) {
+            body.addClass('fixed_top_panel').css('padding-top', $('.header__top').height());
+        } else {
+            body.removeClass('fixed_top_panel').css('padding-top', '0');
+        }
+    }
+
     var mql = window.matchMedia('all and (max-width: 767px)');
     if (mql.matches) {
 
@@ -16,12 +29,155 @@ $(document).ready(function () {
             // TODO: здесь возможно пойдет аякс, поэтому мне return false уже не нужен
             return false;
         })
+
+        // TODO: тут странно
         $('.registration__form input, .registration__form textarea').attr('placeholder', '');
+
+        // запускаем sticky
+        fixedPanel();
+        $(document).scroll(function () {
+            fixedPanel();
+        });
 
     } // end of Resize > 768
 
+    // выпадайка на всплывающей форме "спасибо за регистрацию"
     $('.modal__form-dropdown li').click(function () {
         $(this).parent().toggleClass('active');
     });
+    // маска для телефона
+    body.on('focusin', '.phone', function () {
+        $(this).mask('+7 (999) 999-99-99');
+    });
+
+    // плавный скролл
+    if(window.location.pathname=='/'){
+        $("body").on("click", "a.scroll_link", function (event) {
+            event.preventDefault();
+            var id = $(this.hash);
+            var top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top}, 1500);
+        });
+    }
+
+    // увеличиваем счетчик
+    var show = true;
+    var countbox = '.profit';
+    $(window).on("scroll load resize", function(){
+        if(!show) return false;                   // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop();        // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top;     // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height();        // Высота окна браузера
+        var d_height = $(document).height();      // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if(w_top + 200 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
+            $(".spincrement").spincrement({
+                thousandSeparator: "",
+                duration: 1200
+            });
+            show = false;
+        }
+    });
+
+    // jsDesc: fancybox
+    $(".fancybox").fancybox({
+        buttons: ['close']
+    });
+    $('[data-event]').on('click', function () {
+        if ($(this).data('event') == 'fancy') {
+            $.fancybox.open({
+                src: '#' + $(this).data('name'),
+                slideClass: 'modal_forms',
+            });
+        }
+    });
+
+    // var carouselopts = {
+    //     responsive: [{
+    //         breakpoint: 99999,
+    //         settings: "unslick"
+    //     },{
+    //         breakpoint: 768,
+    //         settings: {
+    //             centerMode: true,
+    //             centerPadding: '25%',
+    //             arrows: false,
+    //             infinite: false
+    //         }
+    //     }]
+    // };
+
+    $('.advantages__slider, .testimonials__wrap').slick({
+        dots: true,
+        autoplay: false,
+        infinite: true,
+        speed: 300,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                    breakpoint: 99999,
+                    settings: "unslick"
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 360,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    // $('.testimonials__wrap').slick({
+    //     dots: true,
+    //     autoplay: false,
+    //     infinite: true,
+    //     speed: 300,
+    //     arrows: false,
+    //     slidesToShow: 4,
+    //     slidesToScroll: 1,
+    //     responsive: [
+    //         {
+    //             breakpoint: 99999,
+    //             settings: "unslick"
+    //         },
+    //         {
+    //             breakpoint: 992,
+    //             settings: {
+    //                 slidesToShow: 3,
+    //                 slidesToScroll: 1
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 500,
+    //             settings: {
+    //                 slidesToShow: 2,
+    //                 slidesToScroll: 1
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 360,
+    //             settings: {
+    //                 slidesToShow: 1,
+    //                 slidesToScroll: 1
+    //             }
+    //         }
+    //     ]
+    // });
 
 });
